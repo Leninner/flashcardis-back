e110b44078bddc95076e154dabb454de9853905d
leninner <lenin@tinkin.one>
2022-09-28 15:27:13 -0500

    Initial commit

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from './../src/app.module'
+import { HTTP_CODES } from '../src/constants/http-codes.utils'
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    await app.init()
+  })
+
+  it('/ (GET)', () =>
+    request(app.getHttpServer())
+      .get('/')
+      .expect(HTTP_CODES.OK)
+      .expect('Hello World!'))
+})
